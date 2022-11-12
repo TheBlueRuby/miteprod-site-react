bd2a5e72fd5064cd49c7ed2a11f71d6d9bfe60d4
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-11-12 22:33:07 +1100

    added basic games page

--- src/games/Games.js
+++ src/games/Games.js
@@ -1,11 +1,42 @@
 function Games() {
     return (
         <div>
-		<h1>Mite Productions - Games</h1>
-            <h2>TheBlueRuby</h2>
-            <ul>
-				<li><a href="https://blueruby.itch.io/">Game Name</a></li>
-			</ul>
+            <h1>Mite Productions - Games</h1>
+
+            <div>
+                <h2>Standalone</h2>
+
+					<h3>Released</h3>
+					<ul>
+						<li>
+							<a target="_blank" rel="noreferrer" href="https://blueruby.itch.io/backrooms-first-contact/">
+								Backrooms First Contact PC - TheBlueRuby
+							</a>
+						</li>
+					</ul>
+
+					<h3>WIP</h3>
+					<ul>
+						<li>
+							<a target="_blank" rel="noreferrer" href="https://miteprod.vercel.app/games/apocdoom/downloads/ApocDoomEp1.wad">
+								Apocalypse Doom - Zaethar
+							</a>
+						</li>
+					</ul>
+
+            </div>
+			<br />
+			<div>
+				<h2>Series</h2>
+				<ul>
+					<li>
+						<a target="_blank" rel="noreferrer" href="/games/ultramite/">
+							Ultramite
+						</a>
+					</li>
+				</ul>
+			</div>
+
         </div>
     );
 }
