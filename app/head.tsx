15d073924d65e411125c0c02394079e4444bb128
TheBlueRuby <109350270+TheBlueRuby@users.noreply.github.com>
2022-11-13 01:15:27 +1100

    added opengraph descriptors

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,24 @@
 export default function Head() {
-  return (
-    <>
-      <title></title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  )
+    return (
+        <>
+            <meta charSet="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <title>Mite Productions Website</title>
+            <meta
+                content="width=device-width, initial-scale=1"
+                name="viewport"
+            />
+            <link rel="icon" href="/favicon.ico" />
+
+            <meta content="Mite Productions - Homepage" property="og:title" />
+            <meta content="website" property="og:type" />
+            <meta
+                content="Website of Mite Productions"
+                property="og:description"
+            />
+            <meta content="/" property="og:url" />
+            <meta content="/images/banner.png" property="og:image" />
+            <meta content="#00BBFF" name="theme-color" />
+        </>
+    );
 }
